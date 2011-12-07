6efb1544995fc63b9a962cb5dd102fdbba038585
Manu Cupcic <cupcicm@gmail.com>
2011-12-07 19:31:41 +0100

    Adds a conky.sh script that displays all windows on the desktop, as a daemon.
    Add this to your startup programs to run conky.

---
+++ conky/conky.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Kill Conky If Running
+test -z "`pgrep conky`" || killall -9 conky
+
+# The directory of conkyrcs
+conky_dir=`readlink -f $(dirname $0)`
+
+# The command for start conkys
+START="conky -d -c"
+
+# The Conkys
+sleep 5
+$START $conky_dir/cpu
+sleep 0.2
+$START $conky_dir/time
+sleep 0.2
+$START $conky_dir/mem
+sleep 0.2
+$START $conky_dir/disk
+sleep 0.2
+$START $conky_dir/net
+sleep 0.2
+$START $conky_dir/big_clock
